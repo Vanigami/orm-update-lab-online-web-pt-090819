237fab829727b8ea94f21dc7b0aa2dd2b50f4e96
Vanna Winland <vgwinland@gmail.com>
2019-11-12 22:10:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,10 +28,17 @@ attr_reader :id
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-      SQL
+    if seld.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id  = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0
+        end
+      end]
 
 
 end
