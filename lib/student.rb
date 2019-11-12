78a6682d177dedb9cca7ed56e556d328f4cc4d9a
Vanna Winland <vgwinland@gmail.com>
2019-11-12 22:49:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,11 @@ attr_reader :id
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE ID NOT EXISTS students
+      CREATE TABLE IF NOT EXISTS students
       (
         id INTEGER PRIMARY KEY,
         name TEXT
-        album TEXT
+        grade TEXT
       )
       SQL
     DB[:conn].execute(sql)
