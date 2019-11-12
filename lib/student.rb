045904257947888cc6c0ccac8484738d4d113b2d
Vanna Winland <vgwinland@gmail.com>
2019-11-12 22:54:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ attr_reader :id
       end
 
       def self.create(name, grade)
-        student = Students.new(name, grade)
+        student = Student.new(name, grade)
         student.save
       end
 
